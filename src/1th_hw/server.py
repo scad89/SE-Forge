42015b329c23947b0cb57a202fd31619cd4cccc8
scad89 <71154471+scad89@users.noreply.github.com>
2021-04-24 04:57:20 +0300

    add body and check headers

--- src/1th_hw/server.py
+++ src/1th_hw/server.py
@@ -1,4 +1,13 @@
 import socket
+import requests
+
+
+def output_head(link):
+    return link.headers
+
+
+def check_body(link):
+    return link.request.body
 
 
 def start_server():
@@ -11,10 +20,15 @@ def start_server():
             client_socket, adress = server.accept()
             data = client_socket.recv(1024).decode('utf-8')
             data = data.split()
+            response = requests.get('https:/'+data[1])
+            headers = output_head(response)
+            body = check_body(response)
+            headers_for_send = str(list(headers.keys())).encode('utf-8')
             HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-            content = f'Method: {data[0]} Path: {data[1]} Params: {data[2]}'.encode(
+            request_line = f'Method: {data[0]} Path: {data[1][1:]} Params: {data[2]} Body: {body}'.encode(
                 'utf-8')
-            client_socket.send(HDRS.encode('utf-8') + content)
+            client_socket.send(HDRS.encode('utf-8') +
+                               request_line + headers_for_send)
             client_socket.shutdown(socket.SHUT_WR)
     except KeyboardInterrupt:
         server.close()
