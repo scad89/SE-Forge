fc64da2ee3545509c3de5b2f9172602e8a66b05e
scad89 <71154471+scad89@users.noreply.github.com>
2021-04-27 18:14:53 +0300

    add right headers http

--- src/1th_hw/server.py
+++ src/1th_hw/server.py
@@ -1,13 +1,31 @@
 import socket
-import requests
+import re
 
 
-def output_head(link):
-    return link.headers
+def output_head(data):
+    dict_head = {}
+    ls = []
+    buf = ''
+    our_key = ''
+    for i in data:
+        if 'HTTP/1.1' in buf:
+            buf = ''
+        if i == '\n':
+            ls.append(buf)
+            buf = ''
+        else:
+            buf += i
 
-
-def check_body(link):
-    return link.request.body
+    for i in ls:
+        for j in range(len(i)):
+            if i[j] == ':':
+                new_key = our_key.replace('\r', '')
+                dict_head[new_key] = i[j+1:]
+                our_key = ''
+                break
+            else:
+                our_key += i[j]
+    return dict_head
 
 
 def start_server():
@@ -19,13 +37,13 @@ def start_server():
         while True:
             client_socket, adress = server.accept()
             data = client_socket.recv(1024).decode('utf-8')
+            headers = output_head(data)
+            print(data)
+            print(adress)
             data = data.split()
-            response = requests.get('https:/'+data[1])
-            headers = output_head(response)
-            body = check_body(response)
             headers_for_send = str(list(headers.keys())).encode('utf-8')
             HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-            request_line = f'Method: {data[0]} Path: {data[1][1:]} Params: {data[2]} Body: {body}'.encode(
+            request_line = f'Method: {data[0]} Path: {data[1][1:]} Params: {data[2]}'.encode(
                 'utf-8')
             client_socket.send(HDRS.encode('utf-8') +
                                request_line + headers_for_send)
