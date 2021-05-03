d753c1d11b26916f51dd2e9426d9dd6756e24e57
scad89 <71154471+scad89@users.noreply.github.com>
2021-05-03 20:13:23 +0300

    fix code

--- src/2th_hw/client.py
+++ src/2th_hw/client.py
@@ -18,41 +18,28 @@ def get_update_data():
             data += chunk
         else:
             break
+    data = data.split('\r\n\r\n')
+    data = ast.literal_eval(data[1])
     with open("data_currency.json", "w") as file:
-        jsonData = json.dumps(data, ensure_ascii=False)
-        file.write(jsonData)
+        json.dump(data, file)
 
 
-def create_list_from_json_string():
-    with open('data.json', 'r') as f:
+def print_all_money():
+    with open('data_currency.json', 'r') as f:
         data = json.loads(f.read())
-    count = 0
     for i in data:
-        if i == '{':
-            break
-        count += 1
-    data = data[count:-1].replace('},{', '},1{').split(',1')
-    return print_all_money(data)
-
-
-def print_all_money(list):
-    for key, value in enumerate(list):
-        list[key] = eval(value)
-    for i in list:
         print("ID ", i["Cur_ID"], '-', "Currency ", i["Cur_Name"], sep=' ')
-    return (list)
+    return data
 
 
 print('''
 Так как сайт нашего Национального Банка организован по принципе"сервис по беларусску" и подключить API с актуальным курсами не представляется
 возможным(https://www.nbrb.by/api/exrates/rates при переходе Not Found), будем выводить код валюты по ID.
 ''')
-
-
 updating_data = input('Требуется ли скачать/обновить данные?(Y/N): ')
 if updating_data.lower() == 'y':
     get_update_data()
-list_money = create_list_from_json_string()
+list_money = print_all_money()
 while True:
     cur_id = int(input('Введите id валюты, которой хотите узнать код: '))
     for i in list_money:
