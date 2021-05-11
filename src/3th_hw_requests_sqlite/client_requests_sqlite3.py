468d7a7a4a51d17f4a04bc66f83e29ad3266c85b
scad89 <71154471+scad89@users.noreply.github.com>
2021-05-11 21:36:19 +0300

    improving_architecture

--- src/3th_hw_requests_sqlite/client_requests_sqlite3.py
+++ src/3th_hw_requests_sqlite/client_requests_sqlite3.py
@@ -22,10 +22,10 @@ def create_database():
         conn.close()
 
 
-def get_rate_for_current_date():
+def get_rate_for_current_data():
     response = requests.get('https://www.nbrb.by/api/exrates/rates/145')
     dict_currency_rate = response.json()
-    return inserting_data_into_database(dict_currency_rate)
+    inserting_data_into_database(dict_currency_rate)
 
 
 def inserting_data_into_database(dict):
@@ -39,22 +39,17 @@ def inserting_data_into_database(dict):
         conn.close()
 
 
-def check_date():
+def get_max_rate_into_data():
     try:
         conn = sqlite3.connect("currencies.db")
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM rates WHERE date = ?",
-                       (str(datetime.now().date()),))
+        cursor.execute(
+            'SELECT currency, date, rate FROM rates WHERE ID = (SELECT MAX(ID) FROM rates)')
         rows = cursor.fetchall()
-        check_return_sql_request(rows)
+        return rows[0][1], rows[0][2], rows[0][3]
     finally:
         conn.commit()
         conn.close()
-
-
-def check_return_sql_request(list):
-    if len(list) == 0:
-        get_rate_for_current_date()
 
 
 def output_current_rate():
@@ -64,25 +59,28 @@ def output_current_rate():
         cursor.execute("SELECT * FROM rates WHERE date = ?",
                        (str(datetime.now().date()),))
         rows = cursor.fetchall()
-        if len(rows) == 0:
-            cursor.execute(
-                'SELECT currency, date, rate FROM rates WHERE ID = (SELECT MAX(ID) FROM rates)')
-            output_rate = cursor.fetchall()
-            return output_rate[0][1], output_rate[0][2], output_rate[0][3]
-        else:
-            return rows[0][1], rows[0][2], rows[0][3]
+        cur, date, rate = check_return_sql_request(rows)
+        return cur, date, rate
     finally:
         conn.commit()
         conn.close()
+
+
+def check_return_sql_request(list):
+    if len(list) == 0:
+        cur, date, rate = get_max_rate_into_data()
+        return cur, date, rate
+    else:
+        return list[0][1], list[0][2], list[0][3]
 
 
 def main():
     questions_about_new_database = input('Создать базу данных?(Y/N): ')
     if questions_about_new_database.lower() == 'y':
         create_database()
-    check_date()
-    name, date, rate = output_current_rate()
-    print(f'Курс {name} на дату {date} составляет {rate} BYN')
+    get_rate_for_current_data()
+    name, data, rate = output_current_rate()
+    print(f'Курс {name} на дату {data} составляет {rate} BYN')
 
 
 if __name__ == '__main__':
